8e6a66bea42a5807d2cbd37b31bfa92511f6eb24
timofeysidorenko03 <timofeysidorenko03@gmail.com>
2023-11-10 11:16:12 +0300

    Testing soft delete from db

--- djangoTask/src/apps/CarDealer/tests.py
+++ djangoTask/src/apps/CarDealer/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from djangoTask.src.apps.Car.models import Brand
+from djangoTask.src.apps.CarDealer.views import BaseViewSet
 
-# Create your tests here.
+
+class BaseViewSetTest(TestCase):
+    def test_perform_destroy(self):
+        obj = Brand.objects.create()
+        view = BaseViewSet()
+        view.perform_destroy(obj)
+        obj.refresh_from_db()  # Update obj from db
+        self.assertFalse(obj.is_active)
+
--- djangoTask/src/apps/User/views.py
+++ djangoTask/src/apps/User/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from djangoTask.src.apps.User.models import User
 from djangoTask.src.apps.User.serializers import UserSerializer
 
 
-class UserViewSet(ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
