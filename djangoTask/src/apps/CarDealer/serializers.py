e601fdfe7ba731dd5c9cd2fd44d961298c4e3a6e
timofeysidorenko03 <timofeysidorenko03@gmail.com>
2023-11-10 12:40:24 +0300

    Add some methods for CarDealerSerializer

--- djangoTask/src/apps/CarDealer/serializers.py
+++ djangoTask/src/apps/CarDealer/serializers.py
@@ -7,22 +7,22 @@ from djangoTask.src.apps.Car.serializers import CarSerializer, SpecificationCarS
 class CarDealerSerializer(serializers.ModelSerializer):
     car_price = serializers.SerializerMethodField()
     car_price_with_discount = serializers.SerializerMethodField()
-    count_of_car = serializers.SerializerMethodField()
+    count = serializers.SerializerMethodField()
     country = CountryField()
     cars = CarSerializer(many=True, required=False)
     specification = SpecificationCarSerializer(many=True, required=False)
 
     def get_car_price(self, obj):
-        supplier_cars = obj.supplier_cars_set.all()
+        supplier_cars = obj.cardealercar_set.all()
         return {car.car_id: car.price for car in supplier_cars}
 
     def get_car_price_with_discount(self, obj):
-        car_dealer_cars = obj.supplier_cars_set.all()
+        car_dealer_cars = obj.cardealercar_set.all()
         return {car.car_id: car.price_with_discount for car in car_dealer_cars}
 
-    def get_count_of_car(self, obj):
-        car_dealer_cars = obj.supplier_cars_set.all()
-        return {car.car_id: car.amount for car in car_dealer_cars}
+    def get_count(self, obj):
+        car_dealer_cars = obj.cardealercar_set.all()
+        return {car.car_id: car.count for car in car_dealer_cars}
 
     class Meta:
         model = CarDealer
@@ -32,12 +32,12 @@ class CarDealerSerializer(serializers.ModelSerializer):
             "country",
             "balance",
             "cars",
-            "specification",
             "car_price",
             "car_price_with_discount",
-            "count_of_car"
+            "count",
+            "specification",
         ]
-        read_only_fields = {
+        read_only_fields = [
             'balance',
             'cars'
-        }
+        ]
