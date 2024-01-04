82d0279cf644e2c48329ec3381e64fa1826c8590
timofeysidorenko03 <timofeysidorenko03@gmail.com>
2024-01-04 14:36:10 +0300

    Add statisctics for car_dealer:unique_clients, unique_suppliers, numbers_of_sells, profit

--- djangoTask/src/apps/Car/serializers.py
+++ djangoTask/src/apps/Car/serializers.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.core.validators import MinValueValidator
+from django.db.models import Count, Sum, F
 from rest_framework import serializers
 from djangoTask.src.apps.Car.models import Car
 from djangoTask.src.apps.CarDealer.models import CarDealer
@@ -31,6 +32,7 @@ class CarDealerUniqueClientsSerializer(serializers.ModelSerializer):
     def get_unique_clients(self, *args, **kwargs):
         return (
             SalesDealerHistory.objects.filter(car_dealer=self.context["car_dealer_id"])
+            .values("car_dealer")
             .values_list("client__user__username", flat=True)
             .distinct()
         )
@@ -46,10 +48,44 @@ class CarDealerUniqueSuppliersSerializer(serializers.ModelSerializer):
     def get_unique_suppliers(self, *args, **kwargs):
         return (
             SupplierSalesHistory.objects.filter(car_dealer=self.context["car_dealer_id"])
-            .values_list("supplier__name", flat=True)
+            .values("car_dealer")
+            .values_list("supplier__supplier_name", flat=True)
             .distinct()
         )
 
     class Meta:
         model = CarDealer
         fields = ("unique_suppliers",)
+
+
+class CarDealerNumberOfSellsSerializer(serializers.ModelSerializer):
+    number_of_sells = serializers.SerializerMethodField()
+
+    def get_number_of_sells(self, *args, **kwargs):
+        return (
+            SalesDealerHistory.objects.filter(car_dealer=self.context["car_dealer_id"])
+            .values("car_dealer")
+            .annotate(Sum("count"))
+            .values_list("count__sum", flat=True)[0]
+        )
+
+    class Meta:
+        model = CarDealer
+        fields = ("number_of_sells",)
+
+
+class CarDealerProfitSerializer(serializers.ModelSerializer):
+    profit = serializers.SerializerMethodField()
+
+    def get_profit(self, *args, **kwargs):
+        return (
+            SalesDealerHistory.objects.filter(car_dealer=self.context["car_dealer_id"])
+            .values("car_dealer")
+            .annotate(profit_sum=Sum(F("count") * F("price")))
+            .values_list("profit_sum", flat=True)[0]
+        )
+
+    class Meta:
+        model = CarDealer
+        fields = ("profit",)
+
--- djangoTask/src/apps/CarDealer/views.py
+++ djangoTask/src/apps/CarDealer/views.py
@@ -12,7 +12,8 @@ from .serializers import CarDealerSerializer
 from .models import CarDealer
 from djangoTask.src.core.tools.permissions import IsCarDealerAdminOrReadOnly
 from ..Car.models import Car, CarDealerCar
-from ..Car.serializers import CarDealerUniqueClientsSerializer, CarDealerUniqueSuppliersSerializer
+from ..Car.serializers import CarDealerUniqueClientsSerializer, CarDealerUniqueSuppliersSerializer, \
+    CarDealerNumberOfSellsSerializer, CarDealerProfitSerializer
 from ...core.tools.mixins import SafeDestroyModelMixin
 
 
@@ -74,6 +75,31 @@ class CarDealerViewSet(viewsets.ModelViewSet, SafeDestroyModelMixin):
         if serializer.is_valid():
             data = serializer.data
             return Response(data, status=status.HTTP_200_OK)
+
+    @action(
+        methods=["get"],
+        detail=True,
+        serializer_class=CarDealerNumberOfSellsSerializer,
+        url_path="number-of-sell",
+    )
+    def get_number_of_sells(self, request, pk=None):
+        serializer = self.get_serializer(data=request.data, context={"car_dealer_id": pk})
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
+
+    @action(
+        methods=["get"],
+        detail=True,
+        serializer_class=CarDealerProfitSerializer,
+        url_path="profit",
+    )
+    def get_showroom_profit(self, request, pk=None):
+        serializer = self.get_serializer(data=request.data, context={"car_dealer_id": pk})
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
+
 
 
 
