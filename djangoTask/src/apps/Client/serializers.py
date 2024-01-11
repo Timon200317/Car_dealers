c152323e0c06575e8d30d2896d106bf08be03664
= <=>
2024-01-11 18:19:34 +0300

    Add create-client action

--- djangoTask/src/apps/Client/serializers.py
+++ djangoTask/src/apps/Client/serializers.py
@@ -1,9 +1,12 @@
+from django.db import IntegrityError
 from django.db.models import Sum, F
 from django_countries.fields import CountryField
 from rest_framework import serializers
 from .models import Client
 from djangoTask.src.apps.Car.serializers import SpecificationCarSerializer
 from ..History.models import SalesDealerHistory
+from ..User.models import User
+from ..User.serializers import UserSerializer
 
 
 class ClientSerializer(serializers.ModelSerializer):
@@ -16,6 +19,35 @@ class ClientSerializer(serializers.ModelSerializer):
         read_only_fields = ["balance", "client_name",
                             "client_second_name", ]
         depth = 1
+
+
+class ClientCreateSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    client_name = serializers.CharField()
+    client_second_name = serializers.CharField()
+    phone_number = serializers.CharField()
+    email = serializers.EmailField()
+    specification = serializers.JSONField()
+
+    def create(self, validated_data):
+        user_id = validated_data.get('user_id')
+        client_name = validated_data.get('client_name')
+        client_second_name = validated_data.get('client_second_name')
+        phone_number = validated_data.pop('phone_number')
+        email = validated_data.pop('email')
+        specification = validated_data.pop('specification')
+        try:
+            client = Client.objects.create(
+                user_id=user_id,
+                client_name=client_name,
+                client_second_name=client_second_name,
+                phone_number=phone_number,
+                email=email,
+                specification=specification,
+            )
+            return client
+        except IntegrityError:
+            raise serializers.ValidationError("This car already exists in this car dealer")
 
 
 class ClientTotalAmountSpentSerializer(serializers.ModelSerializer):
--- djangoTask/src/apps/Client/views.py
+++ djangoTask/src/apps/Client/views.py
@@ -1,17 +1,21 @@
+from django.db import IntegrityError
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, mixins
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
 from .filters import ClientFilter
-from .serializers import ClientSerializer, ClientTotalAmountSpentSerializer, ClientCarsListSerializer
+from .serializers import ClientSerializer, ClientTotalAmountSpentSerializer, ClientCarsListSerializer, \
+    ClientCreateSerializer
 from .models import Client
 from ...core.tools.permissions import IsClientAdminOrReadOnly
 from ...core.tools.mixins import SafeDestroyModelMixin
 
 
-class ClientViewSet(viewsets.ModelViewSet, SafeDestroyModelMixin):
+class ClientViewSet(mixins.ListModelMixin,
+                    GenericViewSet, SafeDestroyModelMixin):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
     permission_classes = (IsClientAdminOrReadOnly,)
@@ -23,6 +27,25 @@ class ClientViewSet(viewsets.ModelViewSet, SafeDestroyModelMixin):
                        'client_second_name',
                        'email',
                        'phone_number']
+
+    @action(
+        detail=True,
+        methods=["post"],
+        url_path="create-client",
+        serializer_class=ClientCreateSerializer,
+    )
+    def create_client(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        except IntegrityError:
+            return Response(
+                {"error": "This client already exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
     @action(
         methods=["get"],
